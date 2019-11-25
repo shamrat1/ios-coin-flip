612db26c8da0cf0eec897f5c5a02f1997f1142ca
Yasin Shamrat <yshamrat@gmail.com>
2019-11-25 14:50:40 +0600

    Initial Commit

---
+++ coinFlip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coinFlip
+//
+//  Created by Leads Computer on 11/25/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
