533e9aeff1b38ad09e4b58a0d8b71a23f525727b
Yasin Shamrat <yshamrat@gmail.com>
2019-11-26 11:36:14 +0600

    single flip transition added to imageView. fade in and out transitions are commented out.

--- coinFlip/ViewController.swift
+++ coinFlip/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,37 +17,42 @@ class ViewController: UIViewController {
     }
 
     @IBAction func filpTheCoin(_ sender: UIButton) {
-        animate()
-        
+//        animate()
+        imageView.isHidden = false
         let random = arc4random_uniform(2)
         if random == 0 {
             imageView.image = UIImage(named: "head")
         }else{
             imageView.image = UIImage(named: "tail")
         }
+    
+        UIView.transition(with: imageView, duration: 0.7, options: [.transitionFlipFromBottom], animations: nil, completion: nil)
+        
     }
     
-    func animate(){
-        imageView.isHidden = false
-        self.imageView.fadeIn(completion: {
-        (finished: Bool) -> Void in
-        self.imageView.fadeOut()
-        })
-    }
+//    func animate(){
+//        imageView.isHidden = false
+//        self.imageView.fadeIn(completion: {
+//        (finished: Bool) -> Void in
+//        self.imageView.fadeOut()
+//        })
+//    }
     
 }
-extension UIView {
-    func fadeIn(duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
-        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
-            self.alpha = 1.0
-        }, completion: completion)
-    }
-
-    func fadeOut(duration: TimeInterval = 1.0, delay: TimeInterval = 1.5, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
-        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
-            self.alpha = 0.0
-        }, completion: completion)
-    }
-}
+//extension UIView {
+//
+//
+//    func fadeIn(duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+//        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
+//            self.alpha = 1.0
+//        }, completion: completion)
+//    }
+//
+//    func fadeOut(duration: TimeInterval = 1.0, delay: TimeInterval = 1.5, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+//        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
+//            self.alpha = 0.0
+//        }, completion: completion)
+//    }
+//}
 
 
